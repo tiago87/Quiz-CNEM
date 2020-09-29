8b9bab8b3af16350d9ad29f0985e96f31625689a
tiago87 <tiagohh87@hotmail.com>
2020-09-29 23:48:44 +0100

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CNEM Quiz</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- MATERIAL DESIGN ICONIC FONT -->
+		<link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+		
+		<!-- STYLE CSS -->
+		<link rel="stylesheet" href="css/style.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+
+	<body>
+
+		<div class="wrapper">
+			<div class="inner">
+				<p style="margin-bottom: 20px;"><i class="fa fa-check-circle" style="font-size:100px;color:rgb(0, 200, 81)"></i></p>
+				<h3 id="numberTest">Respostas registadas com sucesso!</h3>
+			</div>
+		</div>
+	</body>
+</html>
